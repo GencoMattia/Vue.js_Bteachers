173dc1ac7dd9b54596863ba02418779db9bee2b5
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-10 17:07:26 +0200

    Created footer componet

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,9 +9,100 @@ export default {
 </script>
 
 <template>
-    <footer class="footer">
-        <p>&copy; 2024 MyGitHub - <a href="#">Privacy</a> - <a href="#">Terms</a></p>
-    </footer>
+    <footer class=" footer position-absolute bottom-0 end-0">
+
+        <section class="footer-link d-flex justify-content-around mb-3 text-start">
+            <ul>
+                <li>
+                    <h4>Pricing</h4>
+                </li>
+                <li>
+                    <a href="#">
+                        Overview
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Bronze Plan
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Silver Plan
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Gold Plan
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <h4> Company</h4>
+                </li>
+                <li>
+                    <a href="#">
+                        About Us
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Partnerships
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Careers
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Press
+                    </a>
+                </li>
+            </ul>
+
+
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fa-solid fa-location-dot"></i>
+                        Palermo 90100
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Partnerships
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Careers
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Press
+                    </a>
+                </li>
+            </ul>
+        </section>
+
+        <p>&copy; 2024 BTeacher - <a href="#">Privacy</a> - <a href="#">Terms</a></p>
+
+</footer>
 </template>
 
 <style lang="scss" scoped>
@@ -23,6 +114,8 @@ export default {
         padding: 10px 20px;
         text-align: center;
         font-size: 0.8rem;
+        width: 100%;
+        padding: 1.2rem;
 
         a {
             color: $link-color;
@@ -31,6 +124,19 @@ export default {
             &:hover {
             color: $link-hover-color;
             }
+        }
+
+        ul {
+            list-style: none;
+        }
+
+        li a{
+            color: white;
+            width: auto;
+        }
+        
+        a svg{
+            color: white;
         }
     }
 </style>
