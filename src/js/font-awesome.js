437817d1adb43f8c5872b49d62cacc1a9ce44ffc
GencoMattia <gencomattia@hotmail.it>
2024-09-10 17:10:55 +0200

    installed font-awesome npm

---
+++ src/js/font-awesome.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+
+export { FontAwesomeIcon };
