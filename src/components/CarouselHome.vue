517719e4093a7a32a0ab39d8d86048991f799726
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-11 16:40:17 +0200

    fixed carousel button

--- src/components/CarouselHome.vue
+++ src/components/CarouselHome.vue
@@ -45,17 +45,24 @@ export default {
 <template>
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner rounded-4">
-            <CarouselCard class="carousel-item active" v-for="teacher in teachers" :key="teacher.id" :teacher="teacher"/> 
+            <!-- Use `index` to manage the active class for the first item -->
+            <CarouselCard
+                class="carousel-item"
+                :class="{ 'active': index === 0 }" 
+                v-for="(teacher, index) in teachers"
+                :key="teacher.id"
+                :teacher="teacher"
+            />
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control" aria-hidden="true"><font-awesome-icon icon="fa-solid fa-chevron-left" /></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control" aria-hidden="true"><font-awesome-icon icon="fa-solid fa-chevron-right" /></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </template>
 
 <style lang="scss" scoped>
@@ -68,7 +75,7 @@ export default {
         margin-bottom: 3rem;
     }
     span{
-        background-color: rgb(201, 53, 53);
+        color:$link-hover-color;
     }
 
 </style>
